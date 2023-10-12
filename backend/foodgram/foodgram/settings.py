68468824e1b6c93fc42f9db91744d302b633c088
Evsey Kirichkov <evseykirichkov@mail.ru>
2023-10-12 12:02:00 +0300

    commit for 1st review after fix for pep8

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 #     }
 # }
 
-DATABASES = {    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
