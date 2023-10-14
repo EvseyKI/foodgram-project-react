42fe5042bfc9720adbc51349a1d80a9b33435c9a
Evsey Kirichkov <evseykirichkov@mail.ru>
2023-10-15 01:28:11 +0300

    5st

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -4,6 +4,6 @@ from rest_framework.permissions import BasePermission
 class IsAuthorPermissions(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-
-        return request.user == obj.author or request.method not in (
-        'PATCH', 'DELETE')
+        user = request.user
+        author = obj.author
+        return user == author or request.method not in ('PATCH', 'DELETE')
